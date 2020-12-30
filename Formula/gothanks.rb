39a7ecdd423741d6361f823d156597c6d183dff0
Eric Brown <21922+yogieric@users.noreply.github.com>
2020-12-30 21:34:37 +0000

    gothanks: add 0.3.0 bottle.

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -5,6 +5,13 @@ class Gothanks < Formula
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/yogieric/homebrew-foobar/releases/download/gothanks-0.3.0"
+    cellar :any_skip_relocation
+    sha256 "f9992c7538d1260c7e79a42007515e630dd7b0567b077455bd13950b4cb7a380" => :catalina
+    sha256 "74658a03a117deb7f970850839aebe76fd1f85c7bd17f1a58b76bcf8ef28e3ab" => :x86_64_linux
+  end
+
   depends_on "go" => :build
 
   def install
