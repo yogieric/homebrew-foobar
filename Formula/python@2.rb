5f2deac7c3e54d6c230a204be29b30bc97277bb0
Eric (iCloud) Brown <ericbrown@khanacademy.com>
2021-01-01 15:53:21 -0800

    Try to turn on ubuntu again

--- Formula/python@2.rb
+++ Formula/python@2.rb
@@ -9,7 +9,7 @@ class PythonAT2 < Formula
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/2.7.17/Python-2.7.17.tar.xz"
   sha256 "4d43f033cdbd0aa7b7023c81b0e986fd11e653b5248dac9144d508f11812ba41"
-  revision 1
+  revision 2
   head "https://github.com/python/cpython.git", branch: "2.7"
 
   bottle do
@@ -22,12 +22,12 @@ class PythonAT2 < Formula
   depends_on "openssl@1.1"
   depends_on "readline"
   depends_on "sqlite"
-  unless OS.mac?
-    depends_on "linuxbrew/xorg/xorg"
-    depends_on "bzip2"
-    depends_on "ncurses"
-    depends_on "zlib"
-  end
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libffi"
+  uses_from_macos "ncurses"
+  uses_from_macos "unzip"
+  uses_from_macos "zlib"
 
   resource "setuptools" do
     url "https://files.pythonhosted.org/packages/f4/d5/a6c19dcbcbc267aca376558797f036d9bcdff344c9f785fe7d0fe9a5f2a7/setuptools-41.4.0.zip"
